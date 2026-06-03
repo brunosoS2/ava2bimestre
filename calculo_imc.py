2f9a96fda6eda785931bda725b5e24bcd023145a
bibihunter2009-alt <bibihunter2009@gmail.com>
2026-06-03 12:10:27 -0300

    Atualizar o calculo_imc.py

--- calculo_imc.py
+++ calculo_imc.py
@@ -11,19 +11,22 @@ def classificar(valor_imc):
         return "Normal "
 #Ana Julia Campos Bazarin--
 def gerar_aviso(status):
-    if status == "Normmal":
+    if status == "Normal":
         return "Excelente! Seu imc está na faixa ideal."
     else:
         return "Atenção! Seu imc indica que você está acima do peso ideal."
 
     #gabriela cristina.s firmiano
     #Entrada de dados-
+peso = float(input("digite seu peso:"))
+altura = float(input("digite sua altura:"))
+
 resultado = calculo_imc(peso,altura)
 status = classificar(resultado)
-aviso=gerar_viso(status)
+aviso = gerar_viso(status)
 
 print(f"imc:{resultado:.2f}")
-print(f"cassificação:{status}")
-
+print(f"classificação:{status}")
+print(aviso)
 
 
