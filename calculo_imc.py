fb2f03050f4836f03e795ea288d78f05385fa20d
annanerdola-droid <annanerdola@gmail.com>
2026-06-03 13:54:03 +0000

    v.3

--- calculo_imc.py
+++ calculo_imc.py
@@ -9,3 +9,9 @@ def classificar(valor_imc):
         return "Acima do peso"
     else:
         return "Normal "
+#Ana Julia Campos Bazarin--
+def gerar_aviso(status):
+    if status == "Normmal":
+        return "Excelente! Seu imc está na faixa ideal."
+    else:
+        return "Atenção! Seu imc indica que você está acima do peso ideal."
