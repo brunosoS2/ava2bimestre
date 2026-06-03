063a0cd22512181d0c74408b339d2512191b9fbe
maridiascods <polligodoydias@gmail.com>
2026-06-03 13:07:47 +0000

    v2

--- calculo_imc.py
+++ calculo_imc.py
@@ -1,4 +1,11 @@
 #Bruno Sousa--
-def calculo_imc(p,a):
-imc = altura*altura
-resultao = imc / peso
+def calculo_imc(imc):
+    imc = altura * altura
+    resultado = imc / peso
+
+#Mariana Godoy--
+def classificar(valor_imc):
+    if resultado => 25:
+        return "Acima do peso"
+    else:
+        return "Normal "
